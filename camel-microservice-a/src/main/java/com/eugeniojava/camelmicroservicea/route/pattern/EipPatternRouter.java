d8295699071dc1059171c5907cae33dda83f207b
Eugênio Moreira <eugeniojava@outlook.com>
2021-08-26 13:02:52 -0300

    4.24: explore aggregation EIP in Camel

--- camel-microservice-a/src/main/java/com/eugeniojava/camelmicroservicea/route/pattern/EipPatternRouter.java
+++ camel-microservice-a/src/main/java/com/eugeniojava/camelmicroservicea/route/pattern/EipPatternRouter.java
@@ -1,7 +1,12 @@
 package com.eugeniojava.camelmicroservicea.route.pattern;
 
+import com.eugeniojava.camelmicroservicea.CurrencyExchange;
+import java.util.ArrayList;
 import java.util.List;
+import org.apache.camel.AggregationStrategy;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -32,10 +37,48 @@ public class EipPatternRouter extends RouteBuilder {
 //                .to("activemq:split-queue");
 
         // message,message2,message3
-        from("file:files/csv")
-                .convertBodyTo(String.class)
-                .split(method(splitterComponent))
-                .to("activemq:split-queue");
+//        from("file:files/csv")
+//                .convertBodyTo(String.class)
+//                .split(method(splitterComponent))
+//                .to("activemq:split-queue");
+
+        // aggregate
+        // messages -> aggregate -> endpoint
+        // to, 3
+        from("file:files/aggregate-json")
+                .unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
+                .aggregate(simple("${body.getTo}"), new ArrayListAggregationStrategy())
+                .completionSize(3)
+//                .completionTimeout(HIGHEST)
+                .to("log:aggregate-json");
+
+    }
+
+    private static class ArrayListAggregationStrategy implements AggregationStrategy {
+        // 1,2,3
+        // null, 1 -> [1]
+        // [1], 2 -> [1, 2]
+        // [1, 2], 3 -> [1, 2, 3]
+
+        @Override
+        public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+            Object newBody = newExchange.getIn().getBody();
+            ArrayList<Object> list;
+
+            if (oldExchange == null) {
+                list = new ArrayList<>();
+
+                list.add(newBody);
+                newExchange.getIn().setBody(list);
+
+                return newExchange;
+            } else {
+                list = oldExchange.getIn().getBody(ArrayList.class);
+                list.add(newBody);
+
+                return oldExchange;
+            }
+        }
     }
 }
 
